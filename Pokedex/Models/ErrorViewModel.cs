937774444b80cc47d85676587c27300c3d012a8e
Heloez <escolaanaehelo@gmail.com>
2023-04-04 08:47:40 -0300

    projeto pronto falta detalhes

--- Pokedex/Models/ErrorViewModel.cs
+++ Pokedex/Models/ErrorViewModel.cs
@@ -1,8 +1,6 @@
 namespace Pokedex.Models;
-
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
